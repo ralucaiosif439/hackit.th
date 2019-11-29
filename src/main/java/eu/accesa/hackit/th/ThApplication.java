4be624a1762f9ae2c25decffd2dd25a2fe58b257
mihnea.lazar <mihnea.lazar@accesa.eu>
2019-11-29 21:28:49 +0200

    [NoRef-0] Added initial project files

---
+++ src/main/java/eu/accesa/hackit/th/ThApplication.java
@@ -0,0 +1,13 @@
+package eu.accesa.hackit.th;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eu/accesa/hackit/th/ThApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.accesa.hackit.th;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
